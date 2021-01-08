dd52deb0b91378287a719202ca99cc690373adc9
Teya Webster <twebster@lcs.on.ca>
2021-01-08 10:19:02 -0500

    Panda from first day of class.

--- Playground.playground/Pages/My Name.xcplaygroundpage/Contents.swift
+++ Playground.playground/Pages/My Name.xcplaygroundpage/Contents.swift
@@ -4,7 +4,7 @@
  
  Set the size of your desired canvas by adjusting the constants on lines 19 and 20.
  */
-let preferredWidth = 600
+let preferredWidth = 300
 let preferredHeight = 600
 /*:
  ## Required code
@@ -37,7 +37,38 @@ PlaygroundPage.current.liveView = canvas
  */
 
 // Replace this comment with your first comment – what is the goal of the code you're about to write?
+canvas.drawAxes(withScale: true, by: 50)
 canvas.drawRectangle(at: Point(x: 50, y: 75), width: 100, height: 200)
+
+canvas.drawShapesWithBorders = true
+canvas.fillColor = Color.white
+canvas.drawEllipse(at: Point(x: 150, y: 415), width: 150, height: 140)
+
+canvas.fillColor = Color.black
+canvas.drawEllipse(at: Point(x: 125, y: 425), width: 25, height: 25)
+
+canvas.fillColor = Color.white
+canvas.drawEllipse(at: Point(x: 125, y: 420), width: 10, height: 10)
+
+canvas.fillColor = Color.black
+canvas.drawEllipse(at: Point(x: 175, y: 425), width: 25, height: 25)
+
+canvas.fillColor = Color.white
+canvas.drawEllipse(at: Point(x: 175, y: 420), width: 10, height: 10)
+
+canvas.drawCurve(from: Point(x: 145, y: 400), to: Point(x: 155, y: 400), control1: Point(x: 147, y: 405), control2: Point(x: 152, y: 405))
+
+canvas.drawLine(from: Point(x: 150, y: 403), to: Point(x: 150, y: 390))
+
+canvas.drawLine(from: Point(x: 145, y: 385), to: Point(x: 150, y: 390))
+
+canvas.drawLine(from: Point(x: 150, y: 390), to: Point(x: 155, y: 385))
+
+canvas.fillColor = Color.black
+canvas.drawEllipse(at: Point(x: 85, y: 465), width: 40, height: 45)
+
+canvas.drawEllipse(at: Point(x: 215, y: 465), width: 40, height: 45)
+
 
 /*:
  ## Show the Live View
