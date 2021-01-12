52507242078e5673ca6f7f0f1685b211760dd012
Teya Webster <twebster@lcs.on.ca>
2021-01-12 13:37:26 -0500

    All lines now have colours. Background colour is changed to grey

--- Playground.playground/Pages/Geometric Line Pattern.xcplaygroundpage/Contents.swift
+++ Playground.playground/Pages/Geometric Line Pattern.xcplaygroundpage/Contents.swift
@@ -42,118 +42,207 @@ PlaygroundPage.current.liveView = canvas
 canvas.drawAxes(withScale: true, by: 50)
 
 //Background color
-canvas.fillColor = Color.init(hue: 100, saturation: 30, brightness: 80, alpha: 100)
+canvas.fillColor = Color.init(hue: 100, saturation: 100, brightness: 0, alpha: 70)
 canvas.drawRectangle(at: Point(x: 0, y: 0), width: canvas.width, height: canvas.height)
 
 //Lines Bottom Right
+
+//1
 canvas.lineColor = Color.green
 canvas.drawLine(from: Point(x: 0, y: 0), to: Point(x: 600, y: 50))
 
+//2
 canvas.lineColor = Color.yellow
 canvas.drawLine(from: Point(x: 50, y: 0), to: Point(x: 600, y: 100))
 
+//3
 canvas.lineColor = Color.init(hue: 170, saturation: 100, brightness: 100, alpha: 100)
 canvas.drawLine(from: Point(x: 100, y: 0), to: Point(x: 600, y: 150))
 
+//4
 canvas.lineColor = Color.init(hue: 280, saturation: 100, brightness: 50, alpha: 100)
 canvas.drawLine(from: Point(x: 150, y: 0), to: Point(x: 600, y: 200))
 
+//5
 canvas.lineColor = Color.init(hue: 240, saturation: 100, brightness: 45, alpha: 100)
 canvas.drawLine(from: Point(x: 200, y: 0), to: Point(x: 600, y: 250))
 
+//6
 canvas.lineColor = Color.red
 canvas.drawLine(from: Point(x: 250, y: 0), to: Point(x: 600, y: 300))
 
+//7
 canvas.lineColor = Color.init(hue: 220, saturation: 100, brightness: 50, alpha: 100)
 canvas.drawLine(from: Point(x: 300, y: 0), to: Point(x: 600, y: 350))
 
+//8
 canvas.lineColor = Color.init(hue: 359, saturation: 45, brightness: 75, alpha: 100)
 canvas.drawLine(from: Point(x: 350, y: 0), to: Point(x: 600, y: 400))
 
+//9
 canvas.lineColor = Color.init(hue: 140, saturation: 50, brightness: 30, alpha: 100)
 canvas.drawLine(from: Point(x: 400, y: 0), to: Point(x: 600, y: 450))
 
+//10
 canvas.lineColor = Color.init(hue: 240, saturation: 100, brightness: 90, alpha: 100)
 canvas.drawLine(from: Point(x: 450, y: 0), to: Point(x: 600, y: 500))
 
+//11
 canvas.lineColor = Color.yellow
 canvas.drawLine(from: Point(x: 500, y: 0), to: Point(x: 600, y: 550))
 
+//12
 canvas.lineColor = Color.green
 canvas.drawLine(from: Point(x: 550, y: 0), to: Point(x: 600, y: 600))
 
 //Lines Top Left
+
+//1
+canvas.lineColor = Color.green
 canvas.drawLine(from: Point(x: 600, y: 600), to: Point(x: 0, y: 550))
 
+//2
+canvas.lineColor = Color.yellow
 canvas.drawLine(from: Point(x: 550, y: 600), to: Point(x: 0, y: 500))
 
+//3
+canvas.lineColor = Color.init(hue: 170, saturation: 100, brightness: 100, alpha: 100)
 canvas.drawLine(from: Point(x: 500, y: 600), to: Point(x: 0, y: 450))
 
+//4
+canvas.lineColor = Color.init(hue: 280, saturation: 100, brightness: 50, alpha: 100)
 canvas.drawLine(from: Point(x: 450, y: 600), to: Point(x: 0, y: 400))
 
+//5
+canvas.lineColor = Color.init(hue: 240, saturation: 100, brightness: 45, alpha: 100)
 canvas.drawLine(from: Point(x: 400, y: 600), to: Point(x: 0, y: 350))
 
+//6
+canvas.lineColor = Color.red
 canvas.drawLine(from: Point(x: 350, y: 600), to: Point(x: 0, y: 300))
 
+//7
+canvas.lineColor = Color.init(hue: 220, saturation: 100, brightness: 50, alpha: 100)
 canvas.drawLine(from: Point(x: 300, y: 600), to: Point(x: 0, y: 250))
 
+//8
+canvas.lineColor = Color.init(hue: 359, saturation: 45, brightness: 75, alpha: 100)
 canvas.drawLine(from: Point(x: 250, y: 600), to: Point(x: 0, y: 200))
 
+//9
+canvas.lineColor = Color.init(hue: 140, saturation: 50, brightness: 30, alpha: 100)
 canvas.drawLine(from: Point(x: 200, y: 600), to: Point(x: 0, y: 150))
 
+//10
+canvas.lineColor = Color.init(hue: 240, saturation: 100, brightness: 90, alpha: 100)
 canvas.drawLine(from: Point(x: 150, y: 600), to: Point(x: 0, y: 100))
 
+//11
+canvas.lineColor = Color.yellow
 canvas.drawLine(from: Point(x: 100, y: 600), to: Point(x: 0, y: 50))
 
+//12
+canvas.lineColor = Color.green
 canvas.drawLine(from: Point(x: 50, y: 600), to: Point(x: 0, y: 0))
 
 //Lines Bottom Left
+
+//1
+canvas.lineColor = Color.green
 canvas.drawLine(from: Point(x: 0, y: 600), to: Point(x: 50, y: 0))
 
+//2
+canvas.lineColor = Color.yellow
 canvas.drawLine(from: Point(x: 0, y: 550), to: Point(x: 100, y: 0))
 
+//3
+canvas.lineColor = Color.init(hue: 170, saturation: 100, brightness: 100, alpha: 100)
 canvas.drawLine(from: Point(x: 0, y: 500), to: Point(x: 150, y: 0))
 
+//4
+canvas.lineColor = Color.init(hue: 280, saturation: 100, brightness: 50, alpha: 100)
 canvas.drawLine(from: Point(x: 0, y: 450), to: Point(x: 200, y: 0))
 
+//5
+canvas.lineColor = Color.init(hue: 240, saturation: 100, brightness: 45, alpha: 100)
 canvas.drawLine(from: Point(x: 0, y: 400), to: Point(x: 250, y: 0))
 
+//6
+canvas.lineColor = Color.red
 canvas.drawLine(from: Point(x: 0, y: 350), to: Point(x: 300, y: 0))
 
+//7
+canvas.lineColor = Color.init(hue: 220, saturation: 100, brightness: 50, alpha: 100)
 canvas.drawLine(from: Point(x: 0, y: 300), to: Point(x: 350, y: 0))
 
+//8
+canvas.lineColor = Color.init(hue: 359, saturation: 45, brightness: 75, alpha: 100)
 canvas.drawLine(from: Point(x: 0, y: 250), to: Point(x: 400, y: 0))
 
+//9
+canvas.lineColor = Color.init(hue: 140, saturation: 50, brightness: 30, alpha: 100)
 canvas.drawLine(from: Point(x: 0, y: 200), to: Point(x: 450, y: 0))
 
+//10
+canvas.lineColor = Color.init(hue: 240, saturation: 100, brightness: 90, alpha: 100)
 canvas.drawLine(from: Point(x: 0, y: 150), to: Point(x: 500, y: 0))
 
+//11
+canvas.lineColor = Color.yellow
 canvas.drawLine(from: Point(x: 0, y: 100), to: Point(x: 550, y: 0))
 
+//12
+canvas.lineColor = Color.green
 canvas.drawLine(from: Point(x: 0, y: 50), to: Point(x: 600, y: 0))
 
 //Lines Top Right
+
+//1
+canvas.lineColor = Color.green
 canvas.drawLine(from: Point(x: 600, y: 0), to: Point(x: 550, y: 600))
 
+//2
+canvas.lineColor = Color.yellow
 canvas.drawLine(from: Point(x: 600, y: 50), to: Point(x: 500, y: 600))
 
+//3
+canvas.lineColor = Color.init(hue: 170, saturation: 100, brightness: 100, alpha: 100)
 canvas.drawLine(from: Point(x: 600, y: 100), to: Point(x: 450, y: 600))
 
+//4
+canvas.lineColor = Color.init(hue: 280, saturation: 100, brightness: 50, alpha: 100)
 canvas.drawLine(from: Point(x: 600, y: 150), to: Point(x: 400, y: 600))
 
+//5
+canvas.lineColor = Color.init(hue: 240, saturation: 100, brightness: 45, alpha: 100)
 canvas.drawLine(from: Point(x: 600, y: 200), to: Point(x: 350, y: 600))
 
+//6
+canvas.lineColor = Color.red
 canvas.drawLine(from: Point(x: 600, y: 250), to: Point(x: 300, y: 600))
 
+//7
+canvas.lineColor = Color.init(hue: 220, saturation: 100, brightness: 50, alpha: 100)
 canvas.drawLine(from: Point(x: 600, y: 300), to: Point(x: 250, y: 600))
 
+//8
+canvas.lineColor = Color.init(hue: 359, saturation: 45, brightness: 75, alpha: 100)
 canvas.drawLine(from: Point(x: 600, y: 350), to: Point(x: 200, y: 600))
 
+//9
+canvas.lineColor = Color.init(hue: 140, saturation: 50, brightness: 30, alpha: 100)
 canvas.drawLine(from: Point(x: 600, y: 400), to: Point(x: 150, y: 600))
 
+//10
+canvas.lineColor = Color.init(hue: 240, saturation: 100, brightness: 90, alpha: 100)
 canvas.drawLine(from: Point(x: 600, y: 450), to: Point(x: 100, y: 600))
 
+//11
+canvas.lineColor = Color.yellow
 canvas.drawLine(from: Point(x: 600, y: 500), to: Point(x: 50, y: 600))
+
+//12
+canvas.lineColor = Color.green
 canvas.drawLine(from: Point(x: 600, y: 550), to: Point(x: 0, y: 600))
 /*:
  ## Show the Live View
