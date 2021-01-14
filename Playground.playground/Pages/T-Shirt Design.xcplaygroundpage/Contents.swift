48aae8ad18f6bf9ba9f8e6e2c57c7905aca0df22
Teya Webster <twebster@lcs.on.ca>
2021-01-14 16:25:04 -0500

    Fixed mistakes in format of shapes. They are correct now. Finished outline of fox's body. Shapes are rainbow colours to see clearly for now.

--- Playground.playground/Pages/T-Shirt Design.xcplaygroundpage/Contents.swift
+++ Playground.playground/Pages/T-Shirt Design.xcplaygroundpage/Contents.swift
@@ -42,69 +42,201 @@ PlaygroundPage.current.liveView = canvas
 canvas.drawAxes(withScale: true, by: 50)
 
 //adding borders
-canvas.drawShapesWithBorders = true
-canvas.borderColor = Color.white
+//canvas.drawShapesWithBorders = true
+//canvas.borderColor = Color.white
 
-//Right foot + leg
+// back Right foot + leg (shape 1)
+canvas.fillColor = Color.yellow
 
-var verticiesOfSomeShape: [Point] = []
+var verticiesOfShape1: [Point] = []
 
-verticiesOfSomeShape.append(Point(x: 450, y: 0))
-verticiesOfSomeShape.append(Point(x: 485, y: 250))
-verticiesOfSomeShape.append(Point(x: 525, y: 125))
-verticiesOfSomeShape.append(Point(x: 475, y: 25))
-verticiesOfSomeShape.append(Point(x: 500, y: 0))
-verticiesOfSomeShape.append(Point(x: 450, y: 0))
-canvas.drawCustomShape(with: verticiesOfSomeShape)
+verticiesOfShape1.append(Point(x: 450, y: 0))
+verticiesOfShape1.append(Point(x: 485, y: 250))
+verticiesOfShape1.append(Point(x: 525, y: 125))
+verticiesOfShape1.append(Point(x: 475, y: 25))
+verticiesOfShape1.append(Point(x: 500, y: 0))
+verticiesOfShape1.append(Point(x: 450, y: 0))
+canvas.drawCustomShape(with: verticiesOfShape1)
 
-//Left foot + leg
+// back Left foot + leg (shape 2)
 
-verticiesOfSomeShape.append(Point(x: 375, y: 0))
-verticiesOfSomeShape.append(Point(x: 340, y: 250))
-verticiesOfSomeShape.append(Point(x: 300, y: 125))
-verticiesOfSomeShape.append(Point(x: 350, y: 25))
-verticiesOfSomeShape.append(Point(x: 325, y: 0))
-verticiesOfSomeShape.append(Point(x: 375, y: 0))
-canvas.drawCustomShape(with: verticiesOfSomeShape)
 
-//Left front leg
+var verticiesOfShape2: [Point] = []
+verticiesOfShape2.append(Point(x: 375, y: 0))
+verticiesOfShape2.append(Point(x: 340, y: 250))
+verticiesOfShape2.append(Point(x: 300, y: 125))
+verticiesOfShape2.append(Point(x: 350, y: 25))
+verticiesOfShape2.append(Point(x: 325, y: 0))
+verticiesOfShape2.append(Point(x: 375, y: 0))
+canvas.drawCustomShape(with: verticiesOfShape2)
 
-verticiesOfSomeShape.append(Point(x: 375, y: 0))
-verticiesOfSomeShape.append(Point(x: 330, y: 325))
-verticiesOfSomeShape.append(Point(x: 400, y: 250))
-verticiesOfSomeShape.append(Point(x: 400, y: 0))
-verticiesOfSomeShape.append(Point(x: 375, y: 0))
-canvas.drawCustomShape(with: verticiesOfSomeShape)
+//Left front leg (shape 3)
+canvas.fillColor = Color.red
 
-//right front leg
+var verticiesOfShape3: [Point] = []
+verticiesOfShape3.append(Point(x: 375, y: 0))
+verticiesOfShape3.append(Point(x: 330, y: 325))
+verticiesOfShape3.append(Point(x: 400, y: 250))
+verticiesOfShape3.append(Point(x: 400, y: 0))
+verticiesOfShape3.append(Point(x: 375, y: 0))
+canvas.drawCustomShape(with: verticiesOfShape3)
 
-verticiesOfSomeShape.append(Point(x: 425, y: 0))
-verticiesOfSomeShape.append(Point(x: 425, y: 250))
-verticiesOfSomeShape.append(Point(x: 495, y: 325))
-verticiesOfSomeShape.append(Point(x: 450, y: 0))
-verticiesOfSomeShape.append(Point(x: 425, y: 0))
-canvas.drawCustomShape(with: verticiesOfSomeShape)
+//right front leg (shape 4)
+var verticiesOfShape4: [Point] = []
+verticiesOfShape4.append(Point(x: 425, y: 0))
+verticiesOfShape4.append(Point(x: 425, y: 250))
+verticiesOfShape4.append(Point(x: 495, y: 325))
+verticiesOfShape4.append(Point(x: 450, y: 0))
+verticiesOfShape4.append(Point(x: 425, y: 0))
+canvas.drawCustomShape(with: verticiesOfShape4)
 
-//chest + head
+//chest + head (shape 5)
+canvas.fillColor = Color.green
+var verticiesOfShape5: [Point] = []
+verticiesOfShape5.append(Point(x: 495, y: 325))
+verticiesOfShape5.append(Point(x: 450, y: 400))
+verticiesOfShape5.append(Point(x: 500, y: 450))
+verticiesOfShape5.append(Point(x: 485, y: 500))
+verticiesOfShape5.append(Point(x: 450, y: 525))
+verticiesOfShape5.append(Point(x: 412.5, y: 540))
+verticiesOfShape5.append(Point(x: 375, y: 525))
+verticiesOfShape5.append(Point(x: 340, y: 500))
+verticiesOfShape5.append(Point(x: 325, y: 450))
+verticiesOfShape5.append(Point(x: 375, y: 400))
+verticiesOfShape5.append(Point(x: 330, y: 325))
+verticiesOfShape5.append(Point(x: 400, y: 250))
+verticiesOfShape5.append(Point(x: 412.5, y: 240))
+verticiesOfShape5.append(Point(x: 425, y: 250))
+verticiesOfShape5.append(Point(x: 495, y: 325))
+canvas.drawCustomShape(with: verticiesOfShape5)
 
-verticiesOfSomeShape.append(Point(x: 495, y: 325))
-verticiesOfSomeShape.append(Point(x: 450, y: 400))
-verticiesOfSomeShape.append(Point(x: 500, y: 450))
-verticiesOfSomeShape.append(Point(x: 485, y: 500))
-verticiesOfSomeShape.append(Point(x: 450, y: 525))
-verticiesOfSomeShape.append(Point(x: 412.5, y: 540))
-verticiesOfSomeShape.append(Point(x: 375, y: 525))
-verticiesOfSomeShape.append(Point(x: 340, y: 500))
-verticiesOfSomeShape.append(Point(x: 325, y: 450))
-verticiesOfSomeShape.append(Point(x: 375, y: 400))
-verticiesOfSomeShape.append(Point(x: 330, y: 325))
-verticiesOfSomeShape.append(Point(x: 400, y: 250))
-verticiesOfSomeShape.append(Point(x: 412.5, y: 240))
-verticiesOfSomeShape.append(Point(x: 425, y: 250))
-verticiesOfSomeShape.append(Point(x: 495, y: 325))
-canvas.drawCustomShape(with: verticiesOfSomeShape)
+// inbetween front legs (shape 6)
+canvas.fillColor = Color.blue
 
-// inbetween front legs
+var verticiesOfShape6: [Point] = []
+verticiesOfShape6.append(Point(x: 400, y: 10))
+verticiesOfShape6.append(Point(x: 425, y: 10))
+verticiesOfShape6.append(Point(x: 425, y: 250))
+verticiesOfShape6.append(Point(x: 412.5, y: 240))
+verticiesOfShape6.append(Point(x: 400, y: 250))
+verticiesOfShape6.append(Point(x: 400, y: 10))
+canvas.drawCustomShape(with: verticiesOfShape6)
+
+//left ear front (shape 7)
+canvas.fillColor = Color.red
+
+var verticiesOfShape7: [Point] = []
+verticiesOfShape7.append(Point(x: 335, y: 485))
+verticiesOfShape7.append(Point(x: 300, y: 600))
+verticiesOfShape7.append(Point(x: 335, y: 570))
+verticiesOfShape7.append(Point(x: 385, y: 528))
+verticiesOfShape7.append(Point(x: 345, y: 500))
+verticiesOfShape7.append(Point(x: 335, y: 485))
+canvas.drawCustomShape(with: verticiesOfShape7)
+
+//left ear back (shape 8)
+canvas.fillColor = Color.orange
+
+var verticiesOfShape8: [Point] = []
+verticiesOfShape8.append(Point(x: 335, y: 570))
+verticiesOfShape8.append(Point(x: 385, y: 528))
+verticiesOfShape8.append(Point(x: 380, y: 522))
+verticiesOfShape8.append(Point(x: 335, y: 570))
+canvas.drawCustomShape(with: verticiesOfShape8)
+
+//right ear front (shape 9)
+canvas.fillColor = Color.red
+
+var verticiesOfShape9: [Point] = []
+verticiesOfShape9.append(Point(x: 490, y: 485))
+verticiesOfShape9.append(Point(x: 525, y: 600))
+verticiesOfShape9.append(Point(x: 490, y: 570))
+verticiesOfShape9.append(Point(x: 440, y: 528))
+verticiesOfShape9.append(Point(x: 480, y: 500))
+verticiesOfShape9.append(Point(x: 490, y: 485))
+canvas.drawCustomShape(with: verticiesOfShape9)
+
+//right ear back (shape 10)
+canvas.fillColor = Color.orange
+
+var verticiesOfShape10: [Point] = []
+verticiesOfShape10.append(Point(x: 490, y: 570))
+verticiesOfShape10.append(Point(x: 440, y: 528))
+verticiesOfShape10.append(Point(x: 445, y: 522))
+verticiesOfShape10.append(Point(x: 490, y: 570))
+canvas.drawCustomShape(with: verticiesOfShape10)
+
+//front of chest (shape 11)
+canvas.fillColor = Color.purple
+
+var verticiesOfShape11: [Point] = []
+verticiesOfShape11.append(Point(x: 412.5, y: 240))
+verticiesOfShape11.append(Point(x: 425, y: 250))
+verticiesOfShape11.append(Point(x: 475, y: 325))
+verticiesOfShape11.append(Point(x: 412.5, y: 300))
+verticiesOfShape11.append(Point(x: 350, y: 325))
+verticiesOfShape11.append(Point(x: 400, y: 250))
+verticiesOfShape11.append(Point(x: 412.5, y: 240))
+canvas.drawCustomShape(with: verticiesOfShape11)
+
+canvas.drawLine(from: Point(x: 350, y: 325), to: Point(x: 375, y: 400), lineWidth: 1)
+canvas.drawLine(from: Point(x: 475, y: 325), to: Point(x: 450, y: 400), lineWidth: 1)
+
+//tail closest to body (shape 12)
+canvas.fillColor = Color.blue
+
+var verticiesOfShape12: [Point] = []
+verticiesOfShape12.append(Point(x: 337, y: 50))
+verticiesOfShape12.append(Point(x: 275, y: 75))
+verticiesOfShape12.append(Point(x: 300, y: 325))
+verticiesOfShape12.append(Point(x: 335, y: 225))
+verticiesOfShape12.append(Point(x: 300, y: 125))
+verticiesOfShape12.append(Point(x: 337, y: 50))
+canvas.drawCustomShape(with: verticiesOfShape12)
+
+//tail middle bottom (shape 13)
+canvas.fillColor = Color.green
+
+var verticiesOfShape13: [Point] = []
+verticiesOfShape13.append(Point(x: 275, y: 75))
+verticiesOfShape13.append(Point(x: 225, y: 275))
+verticiesOfShape13.append(Point(x: 225, y: 350))
+verticiesOfShape13.append(Point(x: 300, y: 325))
+verticiesOfShape13.append(Point(x: 275, y: 75))
+canvas.drawCustomShape(with: verticiesOfShape13)
+
+//tail middle top left triangle (shape 14)
+canvas.fillColor = Color.purple
+
+var verticiesOfShape14: [Point] = []
+verticiesOfShape14.append(Point(x: 175, y: 450))
+verticiesOfShape14.append(Point(x: 275, y: 400))
+verticiesOfShape14.append(Point(x: 225, y: 350))
+verticiesOfShape14.append(Point(x: 175, y: 450))
+canvas.drawCustomShape(with: verticiesOfShape14)
+
+//tail middle top right triangle (shape 15)
+canvas.fillColor = Color.orange
+
+var verticiesOfShape15: [Point] = []
+verticiesOfShape15.append(Point(x: 225, y: 350))
+verticiesOfShape15.append(Point(x: 275, y: 400))
+verticiesOfShape15.append(Point(x: 300, y: 325))
+verticiesOfShape15.append(Point(x: 225, y: 350))
+canvas.drawCustomShape(with: verticiesOfShape15)
+
+//tail tip (shape 16)
+canvas.fillColor = Color.yellow
+
+var verticiesOfShape16: [Point] = []
+verticiesOfShape16.append(Point(x: 225, y: 275))
+verticiesOfShape16.append(Point(x: 225, y: 350))
+verticiesOfShape16.append(Point(x: 175, y: 450))
+verticiesOfShape16.append(Point(x: 50, y: 325))
+verticiesOfShape16.append(Point(x: 225, y: 275))
+canvas.drawCustomShape(with: verticiesOfShape16)
+
+
 
 /*:
  ## Show the Live View
